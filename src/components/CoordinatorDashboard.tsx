eb0420dfdb75591f5d3bae660c537904edb943f1
Joadson Rocha <62960392+JoadsonRocha@users.noreply.github.com>
2026-08-09 05:16:03 -0400

    Update CoordinatorDashboard.tsx

--- src/components/CoordinatorDashboard.tsx
+++ src/components/CoordinatorDashboard.tsx
@@ -2222,7 +2222,7 @@ export default function CoordinatorDashboard({
             { id: 'metas', label: 'Metas Eleitorais', icon: <Target className="w-4 h-4" /> },
             { id: 'regional_coords', label: 'Coord. Regionais', icon: <ShieldCheck className="w-4 h-4" /> },
             { id: 'teams', label: 'Equipes & Líderes', icon: <Users className="w-4 h-4" /> },
-            { id: 'voters', label: 'Eleitores Geral', icon: <UserCheck className="w-4 h-4" /> },
+            { id: 'voters', label: 'Eleitores Geral', icon: <UserPlus className="w-4 h-4" /> },
             { id: 'agenda', label: 'Agenda', icon: <Calendar className="w-4 h-4" /> },
             { id: 'mapa', label: 'Mapa Regional', icon: <MapIcon className="w-4 h-4" /> },
             { id: 'materials', label: 'Materiais', icon: <Package className="w-4 h-4" /> },
